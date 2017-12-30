9a018071d87400500d30d05b4a048103bc5578d3
Steven Senkowski <scsenkowski@gmail.com>
2017-12-30 19:43:34 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -26,8 +26,8 @@ def initialize(options={})
 end
 
 def save
-  sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})
-
+  sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+  DB[:conn].execute(sql)
 end
 
 def table_name_for_insert
